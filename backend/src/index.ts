a2c9405ed91686defcdc68cb4fac5dce047c9182
AkiraC0de <ravenakira22@gmail.com>
2026-09-08 18:47:41 +0800

    Save local backend files before pull

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express, {Request, Response} from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+import authRoute from "./routes/auth.routes"
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({credentials: true}))
+
+// Endpoints
+app.use("/api/v1/auth", authRoute)
+
+// Fallback endpoint for unkwon route 
+app.use((req: Request, res: Response) => {
+  res.status(400).json({
+    message: "Unknown endpoint.",
+    endpoints: ["api/v1/auth"]
+  })
+})
+
+app.listen(process.env.PORT, () => {
+  console.log("Backend successfully started at PORT: ", process.env.PORT)
+})
