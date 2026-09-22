34db7d1d2618475121f61e51945faa9677d8744a
AkiraC0de <ravenakira22@gmail.com>
2026-09-23 00:55:04 +0800

    fix: upon major changes with database design, some controllers had an error due to changes to requirements

--- backend/src/validations/facility.validations.ts
+++ backend/src/validations/facility.validations.ts
@@ -11,6 +11,8 @@ export const registerFacilitySchema = z.object({
     .string()
     .trim()
     .optional(),
+  maxCapacity: z
+    .number("maxCapacity is required"),
   longitude: z
     .number("longtitude is required.")
     .min(-180)
@@ -26,23 +28,22 @@ export const patchFacilitySchema = z.object({
     .string("name must be a string.")
     .trim()
     .optional(),
-
   address: z
     .string("address must be a string.")
     .trim()
     .optional(),
-
   note: z
     .string("note must be a string.")
     .trim()
     .optional(),
-
+  maxCapacity: z
+    .number("maxCapacity is required")
+    .optional(),
   longitude: z
     .number("longitude must be a number.")
     .min(-180)
     .max(180)
     .optional(),
-
   latitude: z
     .number("latitude must be a number.")
     .min(-90)
